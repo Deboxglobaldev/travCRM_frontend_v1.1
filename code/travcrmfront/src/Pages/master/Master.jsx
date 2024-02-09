39f7c4047dd474e58282b50bf5b01f8be5f25e1d
ansardebox <sanaul.ansari@deboxglobal.com>
2024-02-09 12:34:47 +0530

    new master page templates

--- code/travcrmfront/src/Pages/master/Master.jsx
+++ code/travcrmfront/src/Pages/master/Master.jsx
@@ -8,25 +8,7 @@ const Master = () => {
     <>
       <div className="master container-fluid">
         <div className="row px-2 py-4 bg-light">
-          <MasterData
-            masterList = {masterList.GenralList}
-          />
-          <MasterData
-            masterList = {masterList.HotelList}
-          />
-          <MasterData
-            masterList = {masterList.MonumentList}
-          />
-          <MasterData
-            masterList = {masterList.TransferlList}
-          />
-          <MasterData
-            masterList = {masterList.AdditionList}
-          />
-          <MasterData
-            masterList = {masterList.AdditionList}
-          />
-
+          <MasterData/>
         </div>
       </div>
     </>
--- code/travcrmfront/src/Pages/master/MasterData.jsx
+++ code/travcrmfront/src/Pages/master/MasterData.jsx
@@ -1,23 +1,213 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
 const MasterData = ({ masterList }) => {
   return (
     <>
-      <ul class="col-lg-2 col-md-4 my-1 col-sm-6">
-        {masterList.map((v) => {
-          return (
-            <NavLink to={`/master/${v.Path}`} style={{ color: "red" }}>
-              {
-                <li class="list-group-item">
-                  <i class="fa-solid fa-flag"></i>
-                  {v.MasterName}
-                </li>
-              }
-            </NavLink>
-          );
-        })}
-      </ul>
+      <div className="container-fluid">
+
+        {/* General */}
+        <div className="row bg-light shadow-lg my-2 text-dark">
+          <div className="col-1 py-2">
+              <p className="text-primary fw-500 m-0 mb-1" style={{fontSize:'11px', fontWeight:'500'}}>GENERAL</p>
+              <img src="assets/icons/main-menu.png"
+               alt="main-menu" style={{height:'40px', width:'40px'}}></img>
+          </div>
+          <div className="col-2 mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/countries.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Country Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/process.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Business Type Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col-2 mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/united-states.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>State Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/languages.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Language Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col-2 mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/city.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>City Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/main-menu.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Division Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col-2 mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/conversion-rate.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Lead Source Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/travel-and-tourism.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Tour Type</span>
+                </div>
+              </div>
+          </div>
+          <div className="col-2 mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/destination.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Destination Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/season.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Season Master</span>
+                </div>
+              </div>
+          </div>
+        </div>
+
+        {/* /GENERAL */}
+
+        {/* HOTEL */}
+
+        <div className="row bg-light shadow-lg my-2 text-dark">
+          <div className="col-1 py-2">
+              <p className="text-primary fw-500 m-0 mb-1" style={{fontSize:'11px', fontWeight:'500'}}>HOTEL</p>
+              <img src="assets/icons/hotel.png"
+               alt="main-menu" style={{height:'40px', width:'40px'}}></img>
+          </div>
+          <div className="col mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/document.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Rate Sheet</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/hotel1.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Category Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/living-room.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Room Type Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/hotel3.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Additional Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/amenities.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Amenities Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/room.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Room Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/hotel2.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Type Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/building.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Chain Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/restaurant.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Meal Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/restaurant-1.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Restaurant Master</span>
+                </div>
+              </div>
+          </div>
+          <div className="col mt-4 mb-2 d-flex flex-column justify-content-between">
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/amenities.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="fs-6 pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Hotel Meal Master</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col d-flex align-items-center">
+                    <img src="assets/icons/season.png"
+                     alt="countries" style={{height:'15px', width:'15px'}}/>
+                    <span className="pl-1" style={{fontSize:'8px', fontWeight:'500'}}>Restaurant Master</span>
+                </div>
+              </div>
+          </div>
+        </div>
+
+
+        
+
+      </div>
     </>
   );
 };
