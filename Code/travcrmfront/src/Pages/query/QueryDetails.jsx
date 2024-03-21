ceddc25de69f83b235ae011b9104fee50e6974f4
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2024-03-21 16:02:33 +0530

    update

--- Code/travcrmfront/src/Pages/query/QueryDetails.jsx
+++ Code/travcrmfront/src/Pages/query/QueryDetails.jsx
@@ -1,9 +1,185 @@
 import React from "react";
 
-const QueryDetails= () =>{
-    return(
+const QueryDetails = () => {
+    return (
         <>
-            <h1>This is Query Details</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                    <div className="col-xl-3">
+
+            <div className="card shadow-none border" style={{ marginTop:"10px" }}>
+              <div className="">
+                <div className="row justify-content-between px-3 py-2">
+                  <div className="col-4 border p-0">
+                    <div
+                      className="d-flex align-items-center px-1"
+                      style={{ background: "#f0ffef" }}
+                    >
+                      <div className="w-100">
+                        <a
+                          href="#"
+                          className="text-default font-weight-semibold letter-icon-title"
+                        >
+                          Start City
+                        </a>
+                        <div className="text-muted font-size-sm w-100">
+                          <img
+                            src="/assets/icons/countries.png"
+                            alt="image"
+                            style={{ width: "15px" }}
+                          />
+                          <span className="pl-2">Delhi</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-3 border p-0">
+                    <div
+                      className="d-flex align-items-center px-1"
+                      style={{ background: "#f0ffef" }}
+                    >
+                      <div>
+                        <a
+                          href="#"
+                          className="text-default font-weight-semibold letter-icon-title"
+                        >
+                          Nights
+                        </a>
+                        <div className="text-muted font-size-sm
+                          m-auto w-100
+                        ">
+                          <i
+                            className="fa fa-light fa-moon"
+                            style={{ color: "black", fontSize: "11px" }}
+                          ></i>
+                          <span className="pl-2">2</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-4 border p-0">
+                    <div
+                      className="d-flex align-items-center px-1"
+                      style={{ background: "#f0ffef" }}
+                    >
+                      <div className="w-100">
+                        <a
+                          href="#"
+                          className="text-default font-weight-semibold letter-icon-title"
+                        >
+                          End City
+                        </a>
+                        <div className="text-muted font-size-sm w-100
+                        ">
+                          <img
+                            src="/assets/icons/countries.png"
+                            alt="image"
+                            style={{ width: "15px" }}
+                          />
+                          <span className="pl-2">Mumbai</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                </div>
+              </div>
+              <div className="table-responsive">
+                <table className="table text-nowrap">
+                  <thead>
+                    <tr>
+                      <th className="px-2"></th>
+                      <th className="px-2">Destination</th>
+                      <th className="px-2">Date</th>
+                    </tr>
+                  </thead>
+                  <tbody className="">
+                    <tr>
+                      <td className="px-2">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <a
+                              href="#"
+                              className="text-default font-weight-semibold letter-icon-title"
+                            >
+                              Day 1
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-2">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <a
+                              href="#"
+                              className="text-default font-weight-semibold letter-icon-title"
+                            >
+                              Delhi
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-2">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <a
+                              href="#"
+                              className="text-default font-weight-semibold letter-icon-title"
+                            >
+                              Banglore
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="px-2">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <a
+                              href="#"
+                              className="text-default font-weight-semibold letter-icon-title"
+                            >
+                              Day 2
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-2">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <a
+                              href="#"
+                              className="text-default font-weight-semibold letter-icon-title"
+                            >
+                              Banglore
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-2">
+                        <div className="d-flex align-items-center">
+                          <div>
+                            <a
+                              href="#"
+                              className="text-default font-weight-semibold letter-icon-title"
+                            >
+                              Mumbai
+                            </a>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
--- code/travcrmfront/src/Pages/query/QueryView.jsx
+++ code/travcrmfront/src/Pages/query/QueryView.jsx
@@ -4,7 +4,7 @@ import { NavLink, Outlet, useLocation } from "react-router-dom";
 import CustomizedSteppers from "./CustomizedSteppers";
 
 const QueryView = () => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   const cardBody = {
     MsFlex: "1 1 auto",
     flex: "1 1 auto",
@@ -28,14 +28,18 @@ const QueryView = () => {
             </div>
             <div className="d-flex align-items-center mb-3 mb-sm-0">
               <div className="ml-3">
-                <span className="text-muted">Subject: </span><span style={{ color: "#16b90e" }}>
+                <span className="text-muted">Subject: </span>
+                <span style={{ color: "#16b90e" }}>
                   02-11-2023 DeBox Global Pvt. Ltd.
                 </span>
               </div>
             </div>
             <div className="d-flex align-items-center mb-3 mb-sm-0">
               <div className="ml-3">
-                <span className="text-muted">Status <span className="badge bg-warning-400 ml-auto">Pending</span></span>
+                <span className="text-muted">
+                  Status{" "}
+                  <span className="badge bg-warning-400 ml-auto">Pending</span>
+                </span>
               </div>
             </div>
             <div className="d-flex align-items-center mb-3 mb-sm-0">
@@ -45,54 +49,78 @@ const QueryView = () => {
             </div>
           </div>
 
-          <div className="col-xl-12 pl-2 mt-3" style={{ padding: 0 }}>
-            <div className="col-xl-12 pl-2" style={{ padding: 0 }}>
+            <div className="col-xl-12 px-4" style={{ padding: 0 }}>
               <div className="card shadow-none border">
                 <div className="card-body" style={{ padding: "1px" }}>
-                  <ul className="nav  nav-pills-toolbar nav-justified">
-                    <li className="nav-item border">
-                      <NavLink to="/query_list/queryview/"
-                       className={`nav-link ${pathname==='/query_list/queryview/'?
-                        'Active':'text-dark'}`}>
+                  <ul className="nav nav-pills-toolbar border-bottom nav-justified p-1">
+                    <li className="nav-item rounded-pill">
+                      <NavLink
+                        to="/query_list/queryview/"
+                        className={`nav-link fs-6 py-1 rounded-pill ${
+                          pathname === "/query_list/queryview/"
+                            ? "Active"
+                            : "text-dark"
+                        }`}
+                      >
                         Query
                       </NavLink>
                     </li>
-                    <li className="nav-item border">
-                      <NavLink to="querydetails"
-                        className={`nav-link ${pathname==='/query_list/queryview/querydetails'?
-                         'Active':'text-dark'}`}
+                    <li className="nav-item rounded-pill">
+                      <NavLink
+                        to="querydetails"
+                        className={`nav-link fs-6 py-1 rounded-pill ${
+                          pathname === "/query_list/queryview/querydetails"
+                            ? "Active"
+                            : "text-dark"
+                        }`}
                       >
                         Query Details
                       </NavLink>
                     </li>
-                    <li className="nav-item border">
-                      <NavLink to="quotation"
-                      className={`nav-link ${pathname==='/query_list/queryview/quotation'?
-                      'Active':'text-dark'}`}
+                    <li className="nav-item rounded-pill">
+                      <NavLink
+                        to="quotation"
+                        className={`nav-link fs-6 py-1 rounded-pill ${
+                          pathname === "/query_list/queryview/quotation"
+                            ? "Active"
+                            : "text-dark"
+                        }`}
                       >
                         Quotation
                       </NavLink>
                     </li>
-                    <li className="nav-item border">
-                      <NavLink to="tourextension"
-                      className={`nav-link ${pathname==='/query_list/queryview/tourextension'?
-                      'Active':'text-dark'}`}
+                    <li className="nav-item rounded-pill">
+                      <NavLink
+                        to="tourextension"
+                        className={`nav-link fs-6 py-1 rounded-pill ${
+                          pathname === "/query_list/queryview/tourextension"
+                            ? "Active"
+                            : "text-dark"
+                        }`}
                       >
                         Tour Extension
                       </NavLink>
                     </li>
-                    <li className="nav-item border">
-                      <NavLink to="clientcomm"
-                      className={`nav-link ${pathname==='/query_list/queryview/clientcomm'?
-                      'Active':'text-dark'}`}
+                    <li className="nav-item rounded-pill">
+                      <NavLink
+                        to="clientcomm"
+                        className={`nav-link fs-6 py-1 rounded-pill ${
+                          pathname === "/query_list/queryview/clientcomm"
+                            ? "Active"
+                            : "text-dark"
+                        }`}
                       >
                         Client Communi...
                       </NavLink>
                     </li>
-                    <li className="nav-item border">
-                      <NavLink to="suppliercomm"
-                      className={`nav-link ${pathname==='/query_list/queryview/suppliercomm'?
-                       'Active':'text-dark'}`}
+                    <li className="nav-item rounded-pill">
+                      <NavLink
+                        to="suppliercomm"
+                        className={`nav-link fs-6 py-1 rounded-pill  ${
+                          pathname === "/query_list/queryview/suppliercomm"
+                            ? "Active"
+                            : "text-dark"
+                        }`}
                       >
                         Supplier Communi...
                       </NavLink>
@@ -104,7 +132,6 @@ const QueryView = () => {
                 </div>
               </div>
             </div>
-          </div>
         </div>
       </div>
     </Layout>
